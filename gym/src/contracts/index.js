6907dc1386971d66d2adc2d4dd5f538418d185bd
Adilzhan Slyamgazy <herztard@gmail.com>
2025-05-09 01:13:35 +0500

    env exaple

--- gym/src/contracts/index.js
+++ gym/src/contracts/index.js
@@ -1,14 +1,11 @@
 import GymCoinABI from './GymCoin.json';
 import UserProfileABI from './UserProfile.json';
 
-// Contract addresses on Sepolia testnet
 export const GYM_COIN_ADDRESS = '0x99b18d859995DD60f04F04409122846C2a87E68a';
 export const USER_PROFILE_ADDRESS = '0xAaD9E59E47012aAe2ee3e86231eB1cbF600A63af';
 
-// Contract ABIs
 export const GYM_COIN_ABI = GymCoinABI;
 export const USER_PROFILE_ABI = UserProfileABI;
 
-// Network configuration
 export const CHAIN_ID = 11155111;
 export const NETWORK_NAME = 'Sepolia Testnet'; 
